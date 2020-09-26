7478f666024878ca0692cc4905c2d6d2e53fb5a0
Bence Bacsa <bacsabence00@gmail.com>
2020-09-26 02:06:36 +0200

    File write

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,6 +20,7 @@ namespace UserMaintenance
             InitializeComponent();
             label1.Text = Resource1.FullName;
             button1.Text = Resource1.Add;
+            button2.Text = Resource1.FileWrite;
 
             listBox1.DataSource = users;
             listBox1.ValueMember = "ID";
@@ -32,6 +34,27 @@ namespace UserMaintenance
                 FullName = textBox1.Text
             };
             users.Add(u);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.ShowDialog();
+            if (sfd.FileName==null)
+            {
+                return;
+            }
+            using (StreamWriter sw = new StreamWriter(sfd.FileName, false, Encoding.UTF8))
+            {
+                foreach (var User in users)
+                {
+                    sw.Write(User.ID);
+                    sw.Write(',');
+                    sw.Write(User.FullName);
+                    sw.WriteLine();
+                }
+            }
+
         }
     }
 }
