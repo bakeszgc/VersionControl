332026d952f6232d27a572c58859d62c76491a92
Bence Bacsa <bacsabence00@gmail.com>
2020-10-18 14:56:59 +0200

    06

--- IRF_06v2_EHMF1V/IRF_06v2_EHMF1V/Form1.cs
+++ IRF_06v2_EHMF1V/IRF_06v2_EHMF1V/Form1.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 
 namespace IRF_06v2_EHMF1V
@@ -23,7 +24,10 @@ namespace IRF_06v2_EHMF1V
             InitializeComponent();
             ExchangeRates();
             Feldolgozas();
-            dataGridView1.DataSource = Rates.ToList();
+            DiagramMegjelenit();
+
+            dataGridView1.DataSource = Rates;
+            
         }
 
         private void ExchangeRates()
@@ -58,6 +62,25 @@ namespace IRF_06v2_EHMF1V
                 var value = decimal.Parse(childElement.InnerText);
                 if (unit != 0) rate.Value = value / unit;
             }
+        }
+
+        private void DiagramMegjelenit()
+        {
+            chartRateData.DataSource = Rates;
+
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
         }
     }
 }
