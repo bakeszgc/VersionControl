65961e0f27ff4374be9d58c92f886783848fb00c
Bence Bacsa <bacsabence00@gmail.com>
2020-10-18 13:32:07 +0200

    04

--- IRF_06v2_EHMF1V/IRF_06v2_EHMF1V/Form1.cs
+++ IRF_06v2_EHMF1V/IRF_06v2_EHMF1V/Form1.cs
@@ -1,4 +1,5 @@
-﻿using IRF_06v2_EHMF1V.MnbServiceReference;
+﻿using IRF_06v2_EHMF1V.Entities;
+using IRF_06v2_EHMF1V.MnbServiceReference;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,10 +14,12 @@ namespace IRF_06v2_EHMF1V
 {
     public partial class Form1 : Form
     {
+        BindingList<RateData> Rates = new BindingList<RateData>();
         public Form1()
         {
             InitializeComponent();
             ExchangeRates();
+            dataGridView1.DataSource = Rates.ToList();
         }
 
         private void ExchangeRates()
