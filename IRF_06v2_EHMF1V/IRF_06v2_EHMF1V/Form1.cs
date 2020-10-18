2346745d0c7703c56ba1ba594c03626b2ce831c7
Bence Bacsa <bacsabence00@gmail.com>
2020-10-18 14:16:59 +0200

    05

--- IRF_06v2_EHMF1V/IRF_06v2_EHMF1V/Form1.cs
+++ IRF_06v2_EHMF1V/IRF_06v2_EHMF1V/Form1.cs
@@ -9,16 +9,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace IRF_06v2_EHMF1V
 {
     public partial class Form1 : Form
     {
         BindingList<RateData> Rates = new BindingList<RateData>();
+        private string result;
+
         public Form1()
         {
             InitializeComponent();
             ExchangeRates();
+            Feldolgozas();
             dataGridView1.DataSource = Rates.ToList();
         }
 
@@ -32,8 +36,28 @@ namespace IRF_06v2_EHMF1V
                 endDate="2020-06-30"
             };
             var response = mnbService.GetExchangeRates(request);
-            var result = response.GetExchangeRatesResult;
+            result = response.GetExchangeRatesResult;
+        }
 
+        private void Feldolgozas()
+        {
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                var rate = new RateData();
+                Rates.Add(rate);
+
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0) rate.Value = value / unit;
+            }
         }
     }
 }
