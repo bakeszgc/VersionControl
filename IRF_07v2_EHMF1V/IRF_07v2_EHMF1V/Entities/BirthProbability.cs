02a3042358aa33267f78252d9149702f7d4020ee
Bence Bacsa <bacsabence00@gmail.com>
2020-10-25 13:04:07 +0100

    03

---
+++ IRF_07v2_EHMF1V/IRF_07v2_EHMF1V/Entities/BirthProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRF_07v2_EHMF1V.Entities
+{
+    class BirthProbability
+    {
+        public int Age { get; set; }
+        public int NbrOfChildren { get; set; }
+        public double Prob { get; set; }
+    }
+}
---
+++ IRF_07v2_EHMF1V/IRF_07v2_EHMF1V/Entities/DeathProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRF_07v2_EHMF1V.Entities
+{
+    class DeathProbability
+    {
+        public Gender Gender { get; set; }
+        public int Age { get; set; }
+        public double Prob { get; set; }
+    }
+}
