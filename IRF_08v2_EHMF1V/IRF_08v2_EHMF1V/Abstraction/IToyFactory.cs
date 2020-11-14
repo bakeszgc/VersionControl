ada2f3228f12ccd9c28e673ade578301f96e3afe
Bence Bacsa <bacsabence00@gmail.com>
2020-11-14 16:44:58 +0100

    08

---
+++ IRF_08v2_EHMF1V/IRF_08v2_EHMF1V/Abstraction/IToyFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRF_08v2_EHMF1V.Abstraction
+{
+    public interface IToyFactory
+    {
+        Toy CreateNew();
+    }
+}
