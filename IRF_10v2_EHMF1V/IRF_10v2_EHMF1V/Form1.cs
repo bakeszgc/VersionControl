e0c9bf19369e8be10bf5a88a43a2c14684e810e6
Bence Bacsa <bacsabence00@gmail.com>
2020-11-28 23:20:53 +0100

    07

--- IRF_10v2_EHMF1V/IRF_10v2_EHMF1V/Form1.cs
+++ IRF_10v2_EHMF1V/IRF_10v2_EHMF1V/Form1.cs
@@ -21,6 +21,8 @@ namespace IRF_10v2_EHMF1V
         int nbrOfStepsIncrement = 10;
         int generation = 1;
 
+        Brain winnerBrain = null;
+
         public Form1()
         {
             InitializeComponent();
@@ -63,6 +65,16 @@ namespace IRF_10v2_EHMF1V
                     gc.AddPlayer(b.Mutate());
             }
             gc.Start();
+
+            var winners = from p in topPerformers
+                          where p.IsWinner
+                          select p;
+            if (winners.Count()>0)
+            {
+                winnerBrain = winners.FirstOrDefault().Brain.Clone();
+                gc.GameOver -= Gc_GameOver;
+                return;
+            }
         }
     }
 }
